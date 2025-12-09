2148755f108db6388b9b51bd9d474e8e6a9f3614
Davidterp1 <Davidterp991@gmail.com>
2025-12-09 11:24:39 +0100

    1.0

---
+++ gestio-clinica-medica/src/main/java/com/daw/gestio_clinica_medica/GestioClinicaMedicaApplication.java
@@ -0,0 +1,13 @@
+package com.daw.gestio_clinica_medica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioClinicaMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioClinicaMedicaApplication.class, args);
+	}
+
+}
---
+++ gestio-clinica-medica/src/test/java/com/daw/gestio_clinica_medica/GestioClinicaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw.gestio_clinica_medica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioClinicaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
